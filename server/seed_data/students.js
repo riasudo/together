c4df180cbe520482bb5499f0fbd95dce87994ccd
Ivan Yang <0201ivan@gmail.com>
2020-12-07 17:32:16 -0800

    created sample data

---
+++ server/seed_data/students.js
@@ -0,0 +1,20 @@
+// students have an ID, and each student has a list of programs, and list of instructors 
+
+module.exports = [
+    {
+        name: "Charlie",
+        dob: "2009/01/25",
+        address: "123 example St",
+        city: "Example City",
+        country: "Example Country",
+        phone: "123-456-7890"
+    },
+    {
+        name: "Jane",
+        dob: "2004/01/25",
+        address: "345 example St",
+        city: "Example City",
+        country: "Example Country",
+        phone: "012-345-6789"
+    }
+]
